65388047a9a0ca04585934326bddfbdeac9df7a6
Vignesh D <137488863+vicky-fsd@users.noreply.github.com>
2023-07-28 16:37:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
+
 async function foo() {
-    var res = await fetch("http://api.citybik.es/v2/all")
+    var res = await fetch("https://emojihub.yurace.pro/api/all")
     var result = await res.json();
-    console.log(result.networks);
+    console.log(result);
     var root = document.getElementById("root")
     root.innerHTML=" "
-    for (a in  result.networks) {
-                console.log(a,result.networks[a])
+    for (a in  result) {
+                console.log(a,result[a])
                 var div = document.createElement("div")
                 div.setAttribute("class","col-3 mt-3")
                 div.innerHTML=`
                 <div class="card" style="width: 250px;">
                 <div class="card-body">
-                  <h5 class="card-title">${result.networks[a].name}</h5>
-                  <h6 class="card-subtitle mb-2">${result.networks[a].company}</h6>
-                  <address><i class="fa-solid fa-location-dot fa-bounce" style="color: rgb(6, 184, 184);"></i> ${result.networks[a].location.city}<br>
-                  ${result.networks[a].location.country}<br>
-                  ID : ${result.networks[a].id}<br>
+                  <h5 class="card-title">${result[a].name}</h5>
+                  <h6 class="card-subtitle mb-2">${result[a].category}</h6>
+                  <address>UniCode : ${result[a].unicode[0]}<br>
+                  Group : ${result[a].group}<br>
+                   <h1>${result[a].htmlCode[0]}</h1><br>
                   </address>
-                  <a href="https://citybik.es/" class="btn btn-primary" target="blank">More Info</a>                
+                  <a href="https://emojihub.org/" class="btn btn-primary" target="blank">More Info</a>                
                 </div>
               </div>
               </div>
@@ -26,4 +27,4 @@ async function foo() {
                }
   }
   
-  foo()
+  foo();
